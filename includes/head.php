0b9de725909635463407dc7ac2d99854f174c4fb
Mari Phkhakadze <64760145+marikoosa@users.noreply.github.com>
2020-05-06 16:53:35 -0400

    Create head.php

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<title><?php
+  if ($PAGETITLE == ""){
+    $PAGETITLE = "Please set title with $PAGETITLE = 'x'";
+  }
+  echo $PAGETITLE;
+ ?></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type = "text/css" href="./css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="scripts/jquery-3.5.0.min.js"><\/script>');</script>
+<script src="./scripts/scripts.js"></script>
+<script src="./scripts/jquery.passtrength.js"></script>
+<script src="./scripts/jquery.colorbox.js"></script>
