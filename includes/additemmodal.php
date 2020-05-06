4dc87ec2f5f1d14cd45d8107a54e6344ddbf4299
Mari Phkhakadze <64760145+marikoosa@users.noreply.github.com>
2020-05-06 16:50:16 -0400

    Create additemmodal.php

---
+++ includes/additemmodal.php
@@ -0,0 +1,12 @@
+<div id="modal-window" class="modal">
+  <form class="modal-content-add animate" action="bucketlist.php" method="post">
+    <div class="container">
+      <h1 id="modal-header"> Add list item</h1>
+      <label for="uname"><b>List item title</b></label>
+      <input type="text" placeholder="Enter list item title" name="uname" required>
+      <label for="public-list" i>Make list public</label>
+      <input type="checkbox" name="public" id="make-public" value="1"></br>
+      <button type="submit" name="addto-list" id="addto-list">Add</button>
+    </div>
+  </form>
+</div>
