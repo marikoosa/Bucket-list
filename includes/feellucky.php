bbc893cd5a03713817a1c748a5f1f0eb864e9f86
Mari Phkhakadze <64760145+marikoosa@users.noreply.github.com>
2020-05-06 16:53:14 -0400

    Create feellucky.php

---
+++ includes/feellucky.php
@@ -0,0 +1,24 @@
+<div id="modal-window" class="modal">
+    <div class="modal-content-lucky animate">
+    <div class="container">
+      <h1 id="modal-header">A random list item</h1>
+      <div class="item-container">
+        <div class= "list-item">
+          <div class="item-img">
+            <img src="" class="bucket-image">
+          </div>
+          <div class="item-title">
+            <h1><span id="listtitle"></span></h1>
+            <div class = "icons"></div>
+            <div class="date-completion"><!--completed at-->
+              <p><span>Date completed:</span><span id="listcompletedate"></span></p>
+              <div class="item-description">
+                <p><span>description:</span><span id="listdescription"></span> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
